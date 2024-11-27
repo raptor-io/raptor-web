dbcc67a37d97f182c3133e82e010e95cdceae523
0xDragon888 <lvxuan147@gmail.com>
2024-11-27 21:46:02 +0800

    fix-toml2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -7,9 +5,5 @@ const nextConfig = {
     unoptimized: true,
   },
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await import('@cloudflare/next-on-pages/next-dev').then(mod => mod.setupDevPlatform());
-}
 
 export default nextConfig;
