e65007c848499a693587e8cf560108a5ebb18f9a
0xDragon888 <lvxuan147@gmail.com>
2024-11-27 21:14:44 +0800

    fix-Cloudflare-set

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
 
 export default nextConfig;
+
 if (process.env.NODE_ENV === 'development') {
-    await setupDevPlatform();
-  }
+  await setupDevPlatform();
+}
