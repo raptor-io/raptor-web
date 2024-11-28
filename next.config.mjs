b279feafb23469b5385dc9320d9037c0a86496bc
0xDragon888 <lvxuan147@gmail.com>
2024-11-28 17:01:36 +0800

    fix-Config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' chrome-extension: https: http:; object-src 'self'; style-src 'self' 'unsafe-inline' https:;"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
